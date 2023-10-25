9a26134c588ac345ce2cb494223c543ee762d66e
Ny19BökényiA <Ny19BknyiA@203-11>
2023-10-25 10:13:29 +0200

    Add project files.

---
+++ bolygok/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Text;
+
+
+namespace bolygok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<naprendszer> bolygok = new List<naprendszer>();
+
+            using (var sr = new StreamReader(@"..\..\..\src\solsys.txt", Encoding.UTF8))
+            {
+                while (!sr.EndOfStream)
+                {
+                    bolygok.Add(new naprendszer(sr.ReadLine()));
+                }
+            }
+
+            
+
+            Console.WriteLine($"A naprendszerben {bolygok.Count()} bolygo van");
+
+         
+            Console.WriteLine($"A naprendszerben a bolygoknak atlagosan{1}holdja van");
+            Console.WriteLine($"A legnaygobb terfogatu bolygo a {1}");
+            Console.Write("Írd be a keresett bolygó nevét: ");
+            var bolygonev = Console.ReadLine();
+            if (bolygonev != bolygok)
+            {
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ bolygok/naprendszer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bolygok
+{
+    internal class naprendszer
+    {
+        public string nev {  get; set; }
+        public int holdszam { get; set; }
+        public double arany {  get; set; }
+
+        public naprendszer(string sor)
+        {
+            string[] x = sor.Split(';');
+            nev = x[0];
+            holdszam = int.Parse(x[1]);
+            arany = double.Parse(x[2].Replace('.', ','));
+        }
+    }
+}
